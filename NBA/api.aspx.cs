8b86a6fa7bdb7f441fe8550e7f5ddab3fcf019c0
Dobee27P <sunrakuop@gmail.com>
2024-05-26 21:42:59 +0700

    Add project files.

---
+++ NBA/api.aspx.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+namespace NBA
+{
+    public partial class api : Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetNBA();
+            }
+        }
+
+        private void GetNBA()
+        {
+            List<Team> teams = new List<Team>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("GetAllTeams", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Team team = new Team
+                            {
+                                Id = reader["id"] != DBNull.Value ? Convert.ToInt32(reader["id"]) : 0,
+                                Abbreviation = reader["abbreviation"]?.ToString(),
+                                City = reader["city"]?.ToString(),
+                                Conference = reader["conference"]?.ToString(),
+                                Division = reader["division"]?.ToString(),
+                                Full_Name = reader["full_name"]?.ToString(),
+                                Name = reader["name"]?.ToString(),
+                                Score = reader["score"] != DBNull.Value ? Convert.ToDouble(reader["score"]) : 0.0
+                            };
+                            teams.Add(team);
+                        }
+                    }
+                }
+            }
+
+            string jsonResponse = JsonConvert.SerializeObject(teams);
+            Response.Clear();
+            Response.Write(jsonResponse);
+            Response.End();
+        }
+
+        public class Team
+        {
+            public int Id { get; set; }
+            public string Abbreviation { get; set; }
+            public string City { get; set; }
+            public string Conference { get; set; }
+            public string Division { get; set; }
+            public string Full_Name { get; set; }
+            public string Name { get; set; }
+            public double Score { get; set; }
+        }
+    }
+}
---
+++ NBA/aset/mycode.js
@@ -0,0 +1,155 @@
+﻿$(document).ready(function () {
+    $.post("api.aspx", {
+        action: "thongke"
+    }, function (data) {
+        // Xử lý dữ liệu JSON nhận được
+        console.log(data); // Ví dụ: in ra console
+
+        // Chuyển đổi dữ liệu JSON thành mảng các đối tượng TeamStanding
+        var teamStandings = JSON.parse(data);
+
+        // Tạo mảng các màu sắc cho các cột biểu đồ
+        var colors = ['#FF5733', '#C70039', '#900C3F', '#581845', '#2F4F4F', '#1E90FF'];
+
+        // Tạo bảng HTML
+        var tableHtml = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>Team</th>
+                        <th>Points</th>
+                        <th>Rank</th>
+                    </tr>
+                </thead>
+                <tbody>
+        `;
+
+        // Mảng để lưu tên đội và điểm số cho biểu đồ
+        var teams = [];
+        var points = [];
+
+        // Lặp qua mỗi phần tử trong mảng dữ liệu
+        teamStandings.forEach((item, index) => {
+            tableHtml += `
+                <tr class="team-row" data-index="${index}" data-wins="${item.wins}" data-losses="${item.losses}" data-WinPercentage"${item.WinPercentage}" data-GamesBehind"${item.GamesBehind}" data-Conference="${item.Conference}" data-Division"${item.Division}">
+                    <td>${item.team}</td>
+                    <td>${item.points}</td>
+                    <td>${item.rank}</td>
+                </tr>
+            `;
+            teams.push(item.team);
+            points.push(item.points);
+        });
+
+        tableHtml += `
+                </tbody>
+            </table>
+        `;
+
+        // Đưa bảng HTML vào một div có id là "table-container"
+        $('#table-container').html(tableHtml);
+
+        // Vẽ biểu đồ
+        var ctx = document.getElementById('pointsChart').getContext('2d');
+        var pointsChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: teams,
+                datasets: [{
+                    label: 'Points',
+                    data: points,
+                    backgroundColor: colors,
+                    borderColor: 'rgba(0, 0, 0, 0.5)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false, // Tắt tự động điều chỉnh kích thước
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: false // Ẩn chú thích
+                    },
+                    datalabels: {
+                        anchor: 'end',
+                        align: 'top',
+                        formatter: Math.round,
+                        font: {
+                            weight: 'bold'
+                        }
+                    }
+                }
+            }
+        });
+
+        // Xử lý sự kiện mouseenter và mouseleave cho các dòng trong bảng
+        $('.team-row').mouseenter(function () {
+            // Lấy thông tin về số trận thắng, số trận hòa, số trận thua, số trận đã đấu và số trận còn lại từ thuộc tính data của dòng được hover
+            var TeamID = $(this).find('td:first-child').text(); // Lấy thông tin đội từ cột đầu tiên của dòng
+            var TeamName = $(this).data('TeamName');
+            var Wins = $(this).data('Wins');
+            var Losses = $(this).data('Losses');
+            var WinPercentage = $(this).data('WinPercentage');
+            var GamesBehind = $(this).data('GamesBehind');
+            var Conference = $(this).data('Conference');
+            var Division = $(this).data('Division');
+
+            // Tạo bảng HTML để hiển thị thông tin chi tiết của đội
+            var teamInfoHtml = `
+        <table>
+            <tr>
+                <td>TeamID:</td>
+                <td>${TeamID}</td>
+            </tr>
+            <tr>
+                <td>TeamName:</td>
+                <td>${TeamName}</td>
+            </tr>
+            <tr>
+                <td>Wins:</td>
+                <td>${Wins}</td>
+            </tr>
+            <tr>
+                <td>Losses:</td>
+                <td>${Losses}</td>
+            </tr>
+            <tr>
+                <td>WinPercentage:</td>
+                <td>${WinPercentage}</td>
+            </tr>
+            <tr>
+                <td>GamesBehind:</td>
+                <td>${GamesBehind}</td>
+            </tr>
+            <tr>
+                <td>Conference:</td>
+                <td>${Conference}</td>
+            </tr><tr>
+            <td>Division:</td>
+            <td>${Division}</td>
+        </tr>
+        </table>`;
+
+            // Hiển thị thông tin chi tiết của đội trong div có id "team-details"
+            $('#team-details').html(teamInfoHtml).show();
+
+            // Thêm lớp để đổi màu nền của dòng khi hover
+            $(this).addClass('team-row-hover');
+        }).mouseleave(function () {
+            // Ẩn thông tin chi tiết khi chuột rời khỏi dòng
+            $('#team-details').hide();
+
+            // Xóa lớp đổi màu nền của dòng khi chuột rời khỏi dòng
+            $(this).removeClass('team-row-hover');
+        });
+
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.error("Request failed: " + textStatus + ", " + errorThrown);
+        $('#table-container').html("An error occurred while loading data.");
+    });
+});
